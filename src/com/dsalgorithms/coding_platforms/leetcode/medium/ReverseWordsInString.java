08c6abcf9ff4008bf5c2b8bb60b15831f8adb1f2
sandeepkulkarni <sandeep.kul88@gmail.com>
2023-09-12 10:24:41 +0530

    Add ebooks

--- src/com/dsalgorithms/coding_platforms/leetcode/medium/ReverseWordsInString.java
+++ src/com/dsalgorithms/coding_platforms/leetcode/medium/ReverseWordsInString.java
@@ -1,92 +1,101 @@
 package com.dsalgorithms.coding_platforms.leetcode.medium;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/reverse-words-in-a-string/
  * 151. Reverse Words in a String
  *
  * Given an input string, reverse the string word by word.
-
- For example,
- Given s = "the sky is blue",
- return "blue is sky the".
-------------------------------------------------------------------------------------------------------------
- Two Pass Approach:
- 1. Split input string by space and store in string array
- 2. Reverse string array: by two pointer approach and swap string(words) instead like previous characters
- 3. Loop through array and append to string
-
- One pass approach:
- 1. While iterating the string in reverse order, we keep track of a word’s begin and end position.
- 2. When we are at the beginning of a word, we append it.
-
- Edge Test Cases:
- 1. Input: " "      Output: ""
- 2. Input: "1 "     Output: "1"
- 3. Input: " 1"     Output: "1"
+ * 
+ * For example,
+ * Given s = "the sky is blue",
+ * return "blue is sky the".
  */
 public class ReverseWordsInString {
     public static void main(String[] args) {
         ReverseWordsInString obj = new ReverseWordsInString();
 
         String input = "the sky is blue";
-        System.out.println("Input: "+input);
+        System.out.println("Input: " + input);
 
-        System.out.println("Two Pass Solution:");
-        String reverse = obj.reverseWords2(input);
-        System.out.println(reverse);
-
-        System.out.println("One Pass Solution:");
-        reverse = obj.reverseWords1(input);
+        String reverse = obj.reverseWords(input);
         System.out.println(reverse);
     }
 
-    /**
-     * Two-pass solution: First pass to split the string by spaces into an array of words,
-     *                    then second pass to extract the words in reversed order.
-     */
-    public String reverseWords2(String s) {
-        String[] array = s.split(" ");
-        int i=0, j=array.length-1;
-        while(i < j){
-            String temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+    // Approach 1: Using Built-in functions
+    /*public String reverseWords(String s) {
+        // remove leading and trailing spaces
+        s = s.trim();
+        // split by multiple spaces
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+        return String.join(" ", wordList);
+    }*/
 
-            i++;
-            j--;
-        }
+    // Approach 2: The most straightforward one. Trim the whitespaces, reverse the
+    // whole string and then reverse each word.
+    public StringBuilder trimSpaces(String s) {
+        int left = 0, right = s.length() - 1;
+        // remove leading spaces
+        while (left <= right && s.charAt(left) == ' ')
+            ++left;
 
-        //Convert reversed array to string
+        // remove trailing spaces
+        while (left <= right && s.charAt(right) == ' ')
+            --right;
+
+        // reduce multiple spaces to single one
         StringBuilder sb = new StringBuilder();
-        for(int k=0; k < array.length; k++){
-            sb.append(array[k]);
-            if(k+1 < array.length){
-                sb.append(" ");
+        while (left <= right) {
+            char c = s.charAt(left);
+
+            if (c != ' '){
+                sb.append(c);
+            }else if (sb.charAt(sb.length() - 1) != ' '){
+                sb.append(c);
             }
+
+            ++left;
         }
-        return sb.toString();
+        return sb;
     }
 
-    /**
-     * Can we do it in 1 pass?
-     * One Pass Solution:
-     *      Start from end, While iterating the string in reverse order, we keep track of a word’s begin and end position.
-     *      When we are at the beginning of a word, we append it.
-     */
-    public String reverseWords1(String s){
-        StringBuilder sb = new StringBuilder();
-
-        int j = s.length();
-        for(int i = s.length()-1; i >= 0; i--){
-            if (s.charAt(i) == ' ') {                   //reset end of word pointer
-                j = i;
-            } else if (i == 0 || s.charAt(i - 1) == ' ') {  //append word in string
-                if (sb.length() != 0) {                     //handle: " ", " 1", "1 "
-                    sb.append(' ');
-                }
-                sb.append(s.substring(i, j));
-            }
+    public void reverse(StringBuilder sb, int left, int right) {
+        while (left < right) {
+            char tmp = sb.charAt(left);
+            sb.setCharAt(left++, sb.charAt(right));
+            sb.setCharAt(right--, tmp);
         }
+    }
+
+    public void reverseEachWord(StringBuilder sb) {
+        int n = sb.length();
+        int start = 0, end = 0;
+
+        while (start < n) {
+            // go to the end of the word
+            while (end < n && sb.charAt(end) != ' ')
+                ++end;
+            // reverse the word
+            reverse(sb, start, end - 1);
+            // move to the next word
+            start = end + 1;
+            ++end;
+        }
+    }
+
+    public String reverseWords(String s) {
+        // converst string to string builder
+        // and trim spaces at the same time
+        StringBuilder sb = trimSpaces(s);
+
+        // reverse the whole string
+        reverse(sb, 0, sb.length() - 1);
+
+        // reverse each word
+        reverseEachWord(sb);
+
         return sb.toString();
     }
 }
